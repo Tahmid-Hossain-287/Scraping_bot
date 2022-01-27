0f1d23769864b0717c1bbc09a3596f38475b7000
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2022-01-27 18:42:10 +0600

    the website changed, so some modifications were needed

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class HackerNewsScraping:
         self.soup = BeautifulSoup(self.web_page.text, 'lxml')
 
         self.score = self.soup.find_all(class_="score")
-        self.storylink = self.soup.find_all(class_="storylink", href=True)
+        self.storylink = self.soup.find_all(class_="titlelink", href=True)
         self.athing = self.soup.find_all(class_="athing")
 
 
@@ -45,20 +45,21 @@ class HackerNewsScraping:
         # Using the os module to write and open storage file so that it works on machine with almost all operating system.
         THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) # This code returns the absolute path of the current directory.
         storage_file = os.path.join(THIS_FOLDER, 'news.txt') # This code joins the absolute path of the current directory with the name of the current file.
-        news_file = open(storage_file, "a")
+        # news_file = open(storage_file, "a")
+        with open(storage_file, "a") as news_file:
 
-        for t, s, l in zip(self.title_list, self.score_list, self.link_list):
-            if int(s.split()[0]) > self.minimum_vote and l not in self.final_list: # This condition checks if the score if more than 50 and if the link is not saved previously.
-                # print(f"{t} \n Points: {s} \n Link: {l} ")
-                self.final_list.append(l)
-                news_file.write(f"{t} \n Points: {s} \n Link: {l} \n")
+            for t, s, l in zip(self.title_list, self.score_list, self.link_list):
+                if int(s.split()[0]) > self.minimum_vote and l not in self.final_list: # This condition checks if the score if more than 50 and if the link is not saved previously.
+                    print(f"{t} \n Points: {s} \n Link: {l} ")
+                    self.final_list.append(l)
+                    news_file.write(f"{t} \n Points: {s} \n Link: {l} \n")
                 
-        news_file.close()
+        # news_file.close()
 
     def scrape_upto_the_given_page(self, page_num=0):
         # Give the page_num parameter an integer value and it will scrape pages of hacker news equal to the value of page_num.
 
-        self.page_num = input("Enter the page number you want to scrape from. ")
+        self.page_num = input("Enter the page number upto which you want to scrape from. ")
         while True:
             try:
                 int(self.page_num)
@@ -76,7 +77,7 @@ class HackerNewsScraping:
             try:
                 self.scrape()
                 time.sleep(1.5)
-            except():
+            except:
                 print("No news remaining.")
     
     def scrape_given_page(self):
@@ -96,7 +97,7 @@ class HackerNewsScraping:
 
         try:
             self.scrape()
-        except():
+        except:
             print("No news remaining.")
 
         
